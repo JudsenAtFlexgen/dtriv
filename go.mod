cfdca0eeb269bb94c0aec4acce64bb7de9cd1ec6
Judsen Hembree <judhembree14@gmail.com>
2025-04-28 00:01:31 -0400

    lgtm

--- go.mod
+++ go.mod
@@ -3,7 +3,12 @@ module github.com/JudsenAtFlexgen/dtriv
 go 1.23.4
 
 require (
-	github.com/a-h/templ v0.3.857 // indirect
+	github.com/a-h/templ v0.3.857
+	github.com/gin-gonic/gin v1.10.0
+	github.com/starfederation/datastar v0.21.4
+)
+
+require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -11,7 +16,6 @@ require (
 	github.com/delaneyj/gostar v0.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
@@ -25,7 +29,6 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/samber/lo v1.50.0 // indirect
-	github.com/starfederation/datastar v0.21.4 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
--- src/main.go
+++ src/main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
 	"log"
 	"time"
 
+	"github.com/a-h/templ"
 	"github.com/gin-gonic/gin"
 	datastar "github.com/starfederation/datastar/sdk/go"
+
+	"github.com/JudsenAtFlexgen/dtriv/trivia"
 )
 
 const (
 	port = 9001
 )
+
+type question struct {
+	Quest  string
+	Opts   []string
+	Answer uint
+}
+
+// Needed to convert from templ to gin context
+func render(c *gin.Context, status int, template templ.Component) error {
+	c.Status(status)
+	return template.Render(c.Request.Context(), c.Writer)
+}
+
+// Needed to convert from templ to gin context
+func renderStr(template templ.Component, buf *bytes.Buffer) error {
+	return template.Render(context.Background(), buf)
+}
 
 func main() {
 	r := gin.Default()
@@ -25,6 +47,28 @@ func main() {
 	// Serve the main HTML page
 	r.GET("/", func(c *gin.Context) {
 		c.File("./src/html/index.html")
+	})
+
+	r.GET("/test", func(c *gin.Context) {
+		w := c.Writer
+		r := c.Request
+
+		sse := datastar.NewSSE(w, r)
+		color_bytes := make([]byte, 3)
+		if _, err := rand.Read(color_bytes); err != nil {
+			log.Println("Error generating random bytes:", err)
+			return
+		}
+		hexString := hex.EncodeToString(color_bytes)
+
+		var buf bytes.Buffer
+		style := fmt.Sprintf("color:#%s;border:1px solid #%s;border-radius:0.25rem;padding:1rem;", hexString, hexString)
+		component := trivia.Question(style, "Wowzers")
+		err := renderStr(component, &buf)
+		if err != nil {
+			panic("ohno")
+		}
+		sse.MergeFragments(buf.String())
 	})
 
 	// SSE stream
@@ -48,7 +92,7 @@ func main() {
 					return
 				}
 				hexString := hex.EncodeToString(bytes)
-				frag := fmt.Sprintf(`<span id="feed" style="color:#%s;border:1px solid #%s;border-radius:0.25rem;padding:1rem;">%s</span>`, hexString, hexString, hexString)
+				frag := fmt.Sprintf(`<button id="test" style="color:#%s;border:1px solid #%s;border-radius:0.25rem;padding:1rem;">%s</button>`, hexString, hexString, hexString)
 
 				sse.MergeFragments(frag)
 			}
